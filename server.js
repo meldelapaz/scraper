49ce930f63b0c9b1ef046b30cc79b83e2e082838
meldelapaz <meldelapaz.17@gmail.com>
2017-07-19 21:48:54 -0400

    created server

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var request = require('request');
 var cheerio = require('cheerio');
 var mongoose = require('mongoose');
 
+// Requiring our not and dogs models
+var Note = require("./models/note.js");
+var Article = require("./models/dogs.js");
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
 // Making my port 8080 or process.env.PORT for heroku deployment
 var PORT = process.env.PORT || 8080;
 
@@ -22,6 +29,15 @@ app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 
 // Use all of the static files in the public folder
 app.use(express.static('app/public'));
+
+// Database configuration with mongoose
+mongoose.connect('mongodb://localhost:27017/HomeScraper', { useMongoClient: true });
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+    console.log("Mongoose Error: ", error);
+});
 
 // Listening to the port
 app.listen(PORT, function () {
